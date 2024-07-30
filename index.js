edfb6974fe0c7132d2cebf06aa49a7f979c6a79a
littleninjayt2 <mulajjoel4@gmail.com>
2024-07-30 14:31:14 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+// Define an array of favorite games
+const favoriteGames = [
+    "Fortnite",
+    "Call of Duty",
+    "Roblox",
+    "Valorant",
+    "Apex Legends"
+];
+
+// Function to display favorite games
+function displayFavoriteGames() {
+    // Join the games with commas and display in an alert box
+    alert("My favorite games are: " + favoriteGames.join(", "));
+}
+
+// Call the function to display the games
+displayFavoriteGames();
